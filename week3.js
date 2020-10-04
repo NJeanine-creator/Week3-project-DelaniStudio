f5ab25e7d7079dac9c3250d84e6071a10bf4c2ee
NJeanine-creator <jnishimwe321@daviscollege.com>
2020-10-04 23:05:22 +0200

    Add some changes to the file

--- week3.js
+++ week3.js
@@ -3,25 +3,25 @@ $(document).ready(function() {
   $("#design").click(function(){
     $("#design-showing").toggle();
     $(".hide-design-image").toggle();
-  })
+  });
 
   $("#development").click(function(){
     $("#development-showing").toggle();
     $(".hide-development-image").toggle();
-  })
+  });
 
   $("#productManagement").click(function(){
     $("#product-management-showing").toggle();
     $(".hide-product-management").toggle();
-  })
+  });
 
   //Insert white box upo hover
   $(".Hover").hover(function(){
     $(this).animate({opacity:'1'});
-  },
+  });
   function(){
     $(this).animate({opacity:'0'});
-  })
+  });
 $('.submit').click(function () {
         var Name = $('#mce-FNAME').val();
         var Email = $('#mce-EMAIL').val();
